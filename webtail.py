ef78f184790cf5f7bb14b1fa6662ebf4d8ed6c09
Oskar Blom <oskar.blom@gmail.com>
2013-08-30 20:49:50 +0200

    Try reconnecting when disconnected or on error

--- webtail.py
+++ webtail.py
@@ -21,10 +21,20 @@ def index():
     connected = str(app.connected_mode).lower()
     return render_template("index.html", connected=connected)
 
-def tail(sock):
-    #So we don't need to frame our messages
-    for msg in sock.makefile():
-        fan.fanout(msg)
+def tail():
+    try:
+        app.sock = sock = get_sock()
+
+        #So we don't need to frame our messages
+        for msg in sock.makefile():
+            fan.fanout(msg)
+
+        app.logger.warn("Server disconnected. Retrying in 5 seconds.")
+
+    except socket.error:
+        app.logger.warn("Unable to connect to server. Retrying in 5 seconds.")
+
+    gevent.spawn_later(5, tail)
 
 def get_args():
     if not len(sys.argv) == 3:
@@ -35,10 +45,10 @@ def get_args():
 
     return sys.argv[1:]
 
-def get_sock(host, port):
+def get_sock():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.connect((host, port))
+    sock.connect((app.remote_host, app.remote_port))
 
     return sock
 
@@ -48,14 +58,13 @@ if __name__ == "__main__":
     app.connected_mode = True if host and port else False
     try:
         if app.connected_mode:
-            sock = get_sock(host, port)
-            gevent.spawn(tail, sock)
-        else:
-            sock = None
+            app.remote_host = host
+            app.remote_port = port
+            gevent.spawn(tail)
 
         server = WSGIServer(("", 5000), app)
         server.serve_forever()
     except KeyboardInterrupt:
-        if sock:
-            sock.close()
+        if app.sock:
+            app.sock.close()
         server.close()
